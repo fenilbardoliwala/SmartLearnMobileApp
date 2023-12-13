7329e50368e25731bf470f4ed308cbd7ac6f8410
fenilbardoliwala <fenilbardoliwala1212@gmail.com>
2023-12-13 11:52:39 +0530

    Installtion Changes

--- src/main/java/slappinstallation/AppInstallationTest.java
+++ src/main/java/slappinstallation/AppInstallationTest.java
@@ -1,4 +1,5 @@
 package slappinstallation;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -8,18 +9,25 @@ import org.testng.annotations.Test;
 import slbasereport.BaseTest;
 import java.net.MalformedURLException;
 import java.net.URL;
+import org.openqa.selenium.Rotatable;
+//import org.openqa.selenium.HasFederatedCredentialManagement;
 
-public class AppInstallationTest extends BaseTest {
+public class AppInstallationTest {
     @Test
     public void InstallationAPPTest1() throws MalformedURLException {
-        DesiredCapabilities dc=new DesiredCapabilities();
-        dc.setCapability(MobileCapabilityType.AUTOMATION_NAME,"Appium");
-        dc.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        dc.setCapability(MobileCapabilityType.PLATFORM_VERSION,"9.0");
-        dc.setCapability(MobileCapabilityType.DEVICE_NAME,"Android Emulator");
-        dc.setCapability(MobileCapabilityType.APP,ApkUrl);
-        URL url=new URL("http://127.0.0.1:4725/wd/hub");
-        AndroidDriver<MobileElement> driver = new AndroidDriver<>(url,dc);
-        driver.quit();
+        try {
+            AppiumDriver<MobileElement> appiumDriver;
+            DesiredCapabilities dc = new DesiredCapabilities();
+            dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+            dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+            dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
+            dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+            dc.setCapability(MobileCapabilityType.APP, "D:\\FenilIT\\AndroidTesting\\smartLearn(test).apk");
+           // URL appiumServer = new URL("http://localhost:4725/wd/hub");
+            AppiumDriver<MobileElement>driver=new AndroidDriver<>(new URL("http://localhost:4725/wd/hub"),dc);
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
